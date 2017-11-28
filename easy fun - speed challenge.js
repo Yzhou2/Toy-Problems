2aaa1c7a2003c705ecec018d0c49c0af4e61a6b4
yzhou2 <samandjesslovepie@gmail.com>
2017-11-27 21:36:55 -0700

    done - 7 minutes

---
+++ easy fun - speed challenge.js
@@ -0,0 +1,12 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and
+// last characters of a string. You're given one parameter, except in C, where, to keep the difficulty at the
+// level of the kata, you are given two parameters, the first a buffer with length exactly the same as the second
+// parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+function removeChar(str){
+  var newStr = str.split('')//You got this!
+  newStr.pop();
+  newStr.shift();
+  return newStr.join('');
+}
